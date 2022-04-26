a21aa47b9931baafff292a1fa29a5f258f15b7e2
leonardo <leorsousa05@gmail.com>
2022-04-26 16:56:17 -0300

    Arrays

---
+++ exercicios/03-arrays.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrays</title>
+</head>
+<body>
+    <h1>Usando arrays no PHP</h1>
+    <hr>
+
+    <?php
+    $alunos = ["Maria", "João", "Mônica"];
+
+    $cursos = array("HTML5", "React", "Node.js", "PHP");
+    ?>
+
+    <h3>Acessando os dados</h3>
+    <ul>
+        <li><?=$alunos[2]?></li>
+        <li><?=$alunos[1]?></li>
+    </ul>
+
+    <hr>
+
+    <h2>Array associativo</h2>
+    <?php
+        $curso = [
+            "nome" => "Programador Web",
+            "carga_horaria" => 240,
+            "unidade" => "Penha",
+            "ucs" => 5
+        ]
+    ?>
+
+    <h3>Acessando os dados</h3>
+    <p>
+        O curso de <?=$curso["nome"]?> tem
+        <?=$curso["carga_horaria"]?> horas
+    </p>
+
+
+</body>
+</html>
