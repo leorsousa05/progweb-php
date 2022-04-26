2ccb37532ef7919c0dbadf745012cb3c22e60e72
leonardo <leorsousa05@gmail.com>
2022-04-26 15:51:02 -0300

    Exercício PHP

---
+++ exercicio-1.php
@@ -0,0 +1,32 @@
+<!-- # Exercícios de PHP
+
+## Exercício 01
+Criar **variáveis** e/ou **constantes** para:
+
+- Data de hoje
+- Nome de uma pessoa
+- Idade da pessoa
+- Cidade em que nasceu
+
+Coloque dados nestas variáveis/constantes e faça com que o script mostre no HTML uma mensagem semelhante ao exemplo abaixo:
+
+`Hoje é 26/04/2022. Fulano tem 20 anos e nasceu em São Paulo` -->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício</title>
+</head>
+<body>
+    <?php
+        const data = "26/04/2022";
+        const name = "Leonardo";
+        const age = 18;
+        const city = "São Paulo";
+    ?>
+    <p>Hoje é <?=data?>. <?=name?> tem <?=age?> e nasceu em <?=city?></p>
+</body>
+</html>
